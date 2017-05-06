39b169b237dae11813eca35b9428f49d7175286b
Olga Kuzmina <oskuzm@gmail.com>
2017-05-06 12:00:50 +0800

    price by year linechart

--- visualization.R
+++ visualization.R
@@ -1,3 +1,5 @@
 source("read_data.R")
 
-
+train_by_year <- train %>% group_by(year=year(timestamp), prod_type=product_type) %>% summarise(price=mean(price_doc))
+year_price <- ggplot(train_by_year, aes(year, price)) + geom_line() + facet_grid(. ~ prod_type)
+year_price
