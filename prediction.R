343a6afcc123a7c677c0a319e3a6451fe202dcc3
marat-salikhov <msalikhov@insead.edu>
2017-05-06 11:49:53 +0800

    - A new version of prediction code
    Now generates the submission

--- prediction.R
+++ prediction.R
@@ -1,10 +1,21 @@
 source("read_data.R")
+pacman::p_load(lubridate)
 
-train$year = factor(year(train$timestamp))
-train$month = factor(month(train$timestamp))
+train$year = year(train$timestamp)
+train$month = month(train$timestamp)
 train$build_year = as.integer(train$build_year)
 train = train %>% filter(build_year > 1899 & build_year < 2020 & !is.na(build_year))
 linear_model = lm(price_doc ~ floor + I(floor^2) + full_sq + I(full_sq^2) + product_type + sub_area + year + month + build_year + I(build_year^2), data=train)
 summary(linear_model)
 prediction.lm = data_frame(actual =  linear_model$model$price_doc, predicted = linear_model$fitted.values)
 ggplot(prediction.lm) + geom_point(aes(actual, predicted)) + geom_abline(colour="red")
+
+test = read_csv("data/raw/test.csv")
+test = test %>% mutate(year = year(timestamp), month = month(timestamp), build_year = as.integer(build_year))
+print(nrow(test))
+
+result = data_frame(id = test$id, price_doc = predict(linear_model, test))
+mean_price = mean(train$price_doc)
+result[is.na(result)] = mean_price
+
+write_csv(result, "submission.csv")
